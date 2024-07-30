571e153f38ba31d2b9cfaaf46b164785b8d7a384
Sri Nitish <136550620+Nitish2773@users.noreply.github.com>
2024-07-30 18:19:24 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,19 +1,18 @@
 <?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$from = 'Parax Contact Form'; 
-		$to = 'example@gmail.com'; 
-		$subject = $_POST['subject']; 
-		
-		$body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
-	
-
-	mail($to, $subject, $body, $from) or die("Error!");
-
-	header("location: thank-you.html");
-	
-	}
-	
+if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: Parax Contact Form'; 
+    $to = 'nitishkamisetti123@gmail.com'; // Your email address
+    $subject = $_POST['subject']; 
+    
+    $body = "From: $name\n E-Mail: $email\n Subject: $subject\n Message:\n $message";
+    
+    if (mail($to, $subject, $body, $from)) {
+        header("Location: thank-you.html");
+    } else {
+        echo "Error!";
+    }
+}
 ?>
